df129fdd7ee9adc57d887a29fc8d59c0936d3b8f
Jarek Danielak <danielak.jarek@gmail.com>
2020-08-28 22:24:05 +0200

    Updates

---
+++ precache-manifest.5fdce0d2cea639864d9736cbd5eab7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c8e1ed5f71beb0589c03111608b0ee",
+    "url": "/coach-me-pls/index.html"
+  },
+  {
+    "revision": "1ae0f032584cb0254fcd",
+    "url": "/coach-me-pls/static/css/main.2f025802.chunk.css"
+  },
+  {
+    "revision": "14d8f7d693c07d33ae1d",
+    "url": "/coach-me-pls/static/js/2.f8dc057d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coach-me-pls/static/js/2.f8dc057d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae0f032584cb0254fcd",
+    "url": "/coach-me-pls/static/js/main.5669a8a1.chunk.js"
+  },
+  {
+    "revision": "1a02e05437bf3d3869d7",
+    "url": "/coach-me-pls/static/js/runtime-main.e0cd9c96.js"
+  }
+]);
